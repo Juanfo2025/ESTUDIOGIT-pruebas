6ca4dd5e77d952032d99d11047d27b241fda5d2b
Juan Forero <juanforerod@gmail.com>
2025-08-14 07:12:18 -0500

    feat: gráfico de scores con matplotlib

---
+++ ia_miniproyecto_01/grafico.py
@@ -0,0 +1,10 @@
+﻿import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('ia_miniproyecto_01/datos.csv', encoding='utf-8-sig')
+plt.figure()
+plt.bar(df['nombre'], df['score'])
+plt.title('Score por persona')
+plt.ylabel('Score')
+plt.savefig('ia_miniproyecto_01/grafico_scores.png', bbox_inches='tight')
+print('ok:grafico_scores.png')
