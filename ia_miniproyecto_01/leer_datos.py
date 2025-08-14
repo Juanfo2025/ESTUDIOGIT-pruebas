e09f7e8ea69baf727870442ed5fd3871d8a7082f
Juan Forero <juanforerod@gmail.com>
2025-08-14 07:09:05 -0500

    feat: datos.csv y lector con pandas (utf-8)

---
+++ ia_miniproyecto_01/leer_datos.py
@@ -0,0 +1,5 @@
+﻿import pandas as pd
+df = pd.read_csv('ia_miniproyecto_01/datos.csv', encoding='utf-8-sig')
+print('filas:', len(df))
+print('columnas:', df.shape[1])
+print('promedio_score:', round(df['score'].mean(), 2))
